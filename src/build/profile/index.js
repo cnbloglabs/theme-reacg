42399c249b5e3fb71b2a76d61138d430c4edd2a4
guangzan <guangzan1999@outlook.com>
2023-04-22 14:47:05 +0800

    auto deploy

--- src/build/profile/index.js
+++ src/build/profile/index.js
@@ -16,12 +16,6 @@ import {
   userDetails,
 } from '../../common/constants/links'
 
-function buildAvatar() {
-  $('#blog-news').prepend(
-    `<img class='custom-avatar' src='${avatar}' />`,
-  )
-}
-
 function hideFollowButton() {
   if (!isOwner()) {
     return
@@ -29,22 +23,34 @@ function hideFollowButton() {
   $('#p_b_follow').hide()
 }
 
+function buildAvatar() {
+  return `<img class='custom-avatar' src='${avatar}' />`
+}
+
 function buildInfo() {
   const name = getBlogName()
   const date = getBlogAge()
   const followers = getFollowers()
   const following = getFollowing()
-  const el = `<div class="custom-info">
+
+  return (
+    `<div class="custom-info">
         <a class='custom-name' href="${index}">昵称：${name}</a>
         <a href="${userDetails}">园龄：${date}</a>
         <a href="${followersDetailsUrl}">粉丝：${followers}</a>
         <a href="${followingDetailsUrl}">关注：${following}</a>
     </div>`
-  $('#profile_block').before(el)
+  )
+}
+
+function createProfile() {
+  const avatar = buildAvatar()
+  const info = buildInfo()
+  const profile = $('<div class="custom-profile">').append(avatar).append(info)
+  $('#sidebar_news_content').before(profile)
 }
 
 export default () => {
   hideFollowButton()
-  poll(() => $('#profile_block>a').length, buildAvatar)
-  poll(() => $('#profile_block>a').length, buildInfo)
+  poll(() => $('#profile_block>a').length, createProfile)
 }
