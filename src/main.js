ca3d755bf0bbf4d50dbfee5aa5ae06fa2bf4a276
guangzan <guangzan1999@outlook.com>
2023-04-22 14:15:13 +0800

    auto deploy

--- src/main.js
+++ src/main.js
@@ -128,7 +128,40 @@ createTheme()
   .use(
     charts,
     {
-      enable: false,
+      enable: true,
+      labels: [
+        'Vue',
+        'React',
+        'Flutter',
+        'Java',
+        'NodeJs',
+        'TypeScript',
+        'CSS',
+      ],
+      datasets: [
+        {
+          label: 'My First Chart',
+          data: [65, 59, 90, 81, 56, 55, 40],
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(255, 99, 132)',
+        },
+        {
+          label: 'My Second Dataset',
+          data: [28, 48, 40, 19, 96, 27, 100],
+          fill: true,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgb(54, 162, 235)',
+          pointBackgroundColor: 'rgb(54, 162, 235)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(54, 162, 235)',
+        },
+      ],
     },
   )
   .use(
